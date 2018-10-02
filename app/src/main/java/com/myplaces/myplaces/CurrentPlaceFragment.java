3dedc480cf0ad143653c6a884a33917508fb4317
mariyastash <mariyastashevski@gmail.com>
2018-10-02 22:43:24 +0300

    Place save popups

--- app/src/main/java/com/myplaces/myplaces/CurrentPlaceFragment.java
+++ app/src/main/java/com/myplaces/myplaces/CurrentPlaceFragment.java
@@ -1,11 +1,17 @@
 package com.myplaces.myplaces;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +19,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.w3c.dom.Text;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +31,8 @@ public class CurrentPlaceFragment extends Fragment implements OnMapReadyCallback
     private int mPageIcon = R.drawable.ic_location_on_black_24dp;
     private String mTitle = "Current Location";
 
+    Button mSavePlace_btn;
+
     public CurrentPlaceFragment() {
         // Required empty public constructor
     }
@@ -32,11 +42,36 @@ public class CurrentPlaceFragment extends Fragment implements OnMapReadyCallback
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_maps, container, false);
 
+        mSavePlace_btn = rootView.findViewById(R.id.save_place_popup_btn);
+        mSavePlace_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowDialog();
+            }
+        });
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
         return rootView;
+    }
+
+    public void ShowDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        View dialogView = getLayoutInflater().inflate(R.layout.save_place_dialog, null);
+        TextView locationTv = dialogView.findViewById(R.id.location_tv);
+        TextView placeTitleTv = dialogView.findViewById(R.id.place_title_tv);
+        Button editBtn = dialogView.findViewById(R.id.edit_description_btn);
+        Button shareBtn = dialogView.findViewById(R.id.share_btn);
+        Button saveBtn = dialogView.findViewById(R.id.save_btn);
+
+        RecyclerView recyclerView = dialogView.findViewById(R.id.category_recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        builder.setView(dialogView).show();
     }
 
     @Override
---
+++ app/src/main/java/com/myplaces/myplaces/PlaceAdapter.java
@@ -0,0 +1,19 @@
+package com.myplaces.myplaces;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+/*public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceViewHolder>{
+
+    public class PlaceViewHolder extends RecyclerView.ViewHolder{
+
+        TextView categoryTv;
+
+        public PlaceViewHolder(View itemView) {
+            super(itemView);
+
+            categoryTv.findViewById(R.id.place_category_tv);
+        }
+    }
+}*/
