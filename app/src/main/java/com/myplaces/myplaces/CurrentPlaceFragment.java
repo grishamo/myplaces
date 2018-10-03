58fee1bccf9312dbe7fb2614fdcbdb7cd702d03e
Gregory Mozias <grishamo@gmail.com>
2018-10-03 15:03:24 +0300

    Search Google Places

--- app/src/main/java/com/myplaces/myplaces/CurrentPlaceFragment.java
+++ app/src/main/java/com/myplaces/myplaces/CurrentPlaceFragment.java
@@ -1,12 +1,20 @@
 package com.myplaces.myplaces;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,14 +22,22 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Objects;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CurrentPlaceFragment extends Fragment implements OnMapReadyCallback, IPageFragment {
 
     private GoogleMap mMap;
+    private View rootView;
+    private Button SearchPlaceBtn;
     private int mPageIcon = R.drawable.ic_location_on_black_24dp;
     private String mTitle = "Current Location";
+    int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
 
     public CurrentPlaceFragment() {
         // Required empty public constructor
@@ -30,13 +46,33 @@ public class CurrentPlaceFragment extends Fragment implements OnMapReadyCallback
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.activity_maps, container, false);
+        rootView = inflater.inflate(R.layout.activity_current_place, container, false);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        initSearchPlaceBtn();
+
         return rootView;
+    }
+
+    private void initSearchPlaceBtn()
+    {
+        SearchPlaceBtn = rootView.findViewById(R.id.SearchPlaceBtn);
+        SearchPlaceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN).build(Objects.requireNonNull(getActivity()));
+                    startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+                } catch (GooglePlayServicesRepairableException e) {
+                    // TODO: Handle the error.
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    // TODO: Handle the error.
+                }
+            }
+        });
     }
 
     @Override
@@ -57,5 +93,22 @@ public class CurrentPlaceFragment extends Fragment implements OnMapReadyCallback
     @Override
     public String GetPageTitle() {
         return mTitle;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(getContext(), data);
+                Log.i(mTitle, "Place: " + place.getName());
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(getContext(), data);
+                // TODO: Handle the error.
+                Log.i(mTitle, status.getStatusMessage());
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
     }
 }
